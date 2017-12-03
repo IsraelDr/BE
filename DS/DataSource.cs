3cbb0b9543b0783879e466adadc8e407985b91c4
mosheco110 <mosheco110@gmail.com>
2017-12-03 18:10:20 +0200

    add 4 list of class DataSource

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -8,5 +8,9 @@ namespace DS
 {
     class DataSource
     {
+      static List<BE.Nanny> NannyList =new List<BE.Nanny>();
+      static List<BE.Mother> MotherList =new List<BE.Mother>();
+      static List<BE.Child> ChildList= new List<BE.Child>();
+      static List<BE.Contract> ContractList = new List<BE.Contract>();
     }
 }
