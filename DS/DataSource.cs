ac786be7b278d8ebbb7dd3193958c16477e5e47f
mosheco110 <mosheco110@gmail.com>
2017-12-06 09:03:50 +0200

    DataSource

--- DS/DataSource.cs
+++ DS/DataSource.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
+using System.Reflection;
+using System.IO;
 
 namespace DS
 {
-    class DataSource
+    public class DataSource
     {
-      static List<BE.Nanny> NannyList =new List<BE.Nanny>();
-      static List<BE.Mother> MotherList =new List<BE.Mother>();
-      static List<BE.Child> ChildList= new List<BE.Child>();
-      static List<BE.Contract> ContractList = new List<BE.Contract>();
+         static public List<Nanny> NannyList       = new List<BE.Nanny>() ;  
+         static public List<Mother> MotherList     = new List<BE.Mother>();  
+         static public List<Child> ChildList       = new List<BE.Child>();                                                                                                                                                                                                                                                                                                                                                       
+         static public List<Contract> ContractList = new List<BE.Contract>();
+         
     }
 }
