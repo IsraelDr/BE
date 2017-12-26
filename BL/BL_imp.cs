de4b2d5a15940f8913c3e9fa8bc5dea695962f96
Israel <Israel@github.com>
2017-12-26 13:55:31 +0200

    pull, maps is much faster now

--- BL/BL_imp.cs
+++ BL/BL_imp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Google.Api.Maps.Service;
+//using Google.Api.Maps.Service;
 using GoogleMapsApi.Entities.Common;
 using GoogleMapsApi.Entities.Directions.Request;
 using GoogleMapsApi.Entities.Directions.Response;
